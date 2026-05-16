ed28270bb3cd23cdc63ef2b7d893d289d649ff7f
jaacosta <jaacosta@student.42barcelona.com>
2026-05-16 18:31:13 +0200

    simpleHTML good

--- obj/CGI/CGIHandler.d
+++
@@ -1,6 +0,0 @@
-obj/CGI/CGIHandler.o: src/CGI/CGIHandler.cpp \
-  inc/../inc/CGI/CGIHandler.hpp inc/../inc/request/HTTPRequest.hpp \
-  inc/../inc/parser/LocationConfig.hpp \
-  inc/../inc/parser/ServerConfig.hpp inc/parser/LocationConfig.hpp \
-  inc/../inc/utils/CGIUtils.hpp inc/../inc/response/HTTPResponse.hpp \
-  inc/../inc/utils/HTTPStatus.hpp inc/../inc/mime/MimeTypes.hpp
